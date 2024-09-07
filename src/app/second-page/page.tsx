c105fe8e351256a5b971939239e3845a24cb11b2
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-07 13:32:30 +0000

    Added navbar on second page

---
+++ src/app/second-page/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
+
+function SecondPage() {
+    return (
+        <>
+            <Navbar></Navbar>
+        </>
+    )
+}
+
+export default SecondPage;
