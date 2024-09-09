625345204f3f7ab7fafb169cb78f0d3dfcc7ba19
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-09 16:54:10 +0000

    Added suppliers section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import HomeAndOutdoor from "@/components/HomeAndOutdoor/HomeAndOutdoor";
 import Navbar from "@/components/Navbar/Navbar";
 import QuoteForm from "@/components/QuoteForm/QuoteForm";
 import RecommendedItems from "@/components/Recommended/Recommended";
+import SuppliersByRegion from "@/components/Suppliers/Suppliers";
 
 export default function Home() {
   return (
@@ -18,6 +19,7 @@ export default function Home() {
       <QuoteForm></QuoteForm>
       <RecommendedItems></RecommendedItems>
       <ExtraServices></ExtraServices>
+      <SuppliersByRegion></SuppliersByRegion>
     </div>
   );
 }
