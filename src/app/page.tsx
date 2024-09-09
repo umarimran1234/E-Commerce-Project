1f8034d129be90e9a9019d76af9654ac3bd36de3
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-09 16:57:18 +0000

    Added footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Banner from "@/components/Banner/Banner";
 import DealsSection from "@/components/DealsSection/DealsSection";
 import ExtraServices from "@/components/ExtraServices/ExtraServices";
+import Footer from "@/components/Footer/Footer";
 import ConsumerElectronics from "@/components/HomeAndOutdoor/ConsumerElectronics";
 import HomeAndOutdoor from "@/components/HomeAndOutdoor/HomeAndOutdoor";
 import Navbar from "@/components/Navbar/Navbar";
 import QuoteForm from "@/components/QuoteForm/QuoteForm";
 import RecommendedItems from "@/components/Recommended/Recommended";
+import Subscribe from "@/components/Subscribe/Subscribe";
 import SuppliersByRegion from "@/components/Suppliers/Suppliers";
 
 export default function Home() {
@@ -20,6 +22,8 @@ export default function Home() {
       <RecommendedItems></RecommendedItems>
       <ExtraServices></ExtraServices>
       <SuppliersByRegion></SuppliersByRegion>
+      <Subscribe></Subscribe>
+      <Footer></Footer>
     </div>
   );
 }
