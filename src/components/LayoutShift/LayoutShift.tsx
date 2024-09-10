4679f737ee21d65b8cb5239386d390da3621091e
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-10 06:31:00 +0000

    Added layout shift on second page

--- src/components/LayoutShift/LayoutShift.tsx
+++ src/components/LayoutShift/LayoutShift.tsx
@@ -1,6 +1,15 @@
-import React from 'react';
+"use client"
+import React, { useState } from 'react';
 import { IoGridSharp, IoReorderThreeOutline } from 'react-icons/io5';
-import ProductCard from '../Products/ProductCard';
+
+interface ProductProps {
+    name: string;
+    image: string;
+    price: number;
+    rating: number;
+    orders: number;
+    description: string;
+}
 
 const products = [
     {
@@ -31,6 +40,20 @@ const products = [
 ];
 
 function LayoutShift() {
+    const [isGrid, setIsGrid] = useState(false)
+
+    const handleGrid = () => {
+        // console.log("Grid Clicked")
+        setIsGrid(true)
+    }
+
+    const handleNormal = () => {
+        // console.log("Normal Layout")
+        setIsGrid(false)
+    }
+
+    // console.log(isGrid)
+
     return (
         <>
             <div className='container mx-auto'>
@@ -46,19 +69,44 @@ function LayoutShift() {
                                 <option value="">Featured</option>
                                 {/* Add more options as needed */}
                             </select>
-                            <span className='p-1 md:text-xl rounded-lg border-2'><IoGridSharp /></span>
-                            <span className='md:text-xl border-2 rounded-lg p-1'><IoReorderThreeOutline /></span>
+                            <button onClick={() => handleGrid()} className={`${isGrid ? "p-1 md:text-xl bg-gray-300 rounded-lg border-2" : "p-1 md:text-xl rounded-lg border-2"}`}><IoGridSharp /></button>
+                            <button onClick={() => handleNormal()} className={`${!isGrid ? "p-1 md:text-xl bg-gray-300 rounded-lg border-2" : "p-1 md:text-xl rounded-lg border-2"}`}><IoReorderThreeOutline /></button>
                         </div>
                     </div>
                 </div>
             </div>
             <div className="container mx-auto mt-10">
-            <div className="grid gap-8 grid-cols-1">
-                {products.map((product) => (
-                    <ProductCard key={product.name} {...product} />
-                ))}
+                <div className={`grid ${!isGrid ? "gap-8 grid-cols-1" : "lg:grid-cols-3 md:grid-cols-2 gap-4"}`}>
+                    {products.map((product, index) => (
+                        <div key={index} className="bg-white border-2 border-gray-300 rounded-lg shadow-md overflow-hidden">
+                            <div className={`flex ${!isGrid ? "md:flex-row flex-col" : "flex-col"}`}>
+                                <img src={product?.image} alt={product?.name} className={`${!isGrid ? "w-fit mx-auto md:mx-0 h-48 object-cover" : ""}`} />
+                                <div className="p-4">
+                                    <h2 className="text-lg font-bold">{product?.name}</h2>
+                                    <div className="flex md:flex-row flex-col justify-between">
+                                        <div>
+                                            <p className="text-2xl mt-2 font-semibold text-blue-500">${product?.price.toFixed(2)}</p>
+                                            <p className="text-gray-400">
+                                                <span className="text-yellow-500">★★★★★</span> {product?.rating}
+                                            </p>
+                                        </div>
+                                        <div className='flex gap-2 pt-6'>
+                                            <p className="text-gray-400">Orders: {product?.orders}</p>
+                                            <p className='text-[#00B517]'>Free Shipping</p>
+                                        </div>
+                                    </div>
+                                    <div className='mt-2'>
+                                        <p className="text-gray-500">{product?.description}</p>
+                                        <button className="font-medium text-[#0D6EFD]">
+                                            View Details
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
-        </div>
         </>
     )
 }
