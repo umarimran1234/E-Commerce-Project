3c2bb22812c3d3942b1f5e8ca280c3d599b00425
umarimran1234 <umartkd989@gmail.com>
2024-09-10 22:07:02 +0500

    update

--- src/app/second-page/page.tsx
+++ src/app/second-page/page.tsx
@@ -12,30 +12,14 @@ function SecondPage() {
       <div className="flex md:gap-8">
         <FilterSidebar />
         <div className="md:w-9/12 w-8/12">
-          <TopSection />
-          <Products />
-          <Navigation />
+          <LayoutShift></LayoutShift>
+          <Navigation></Navigation>
         </div>
       </div>
-      <Subscribe />
+      <Subscribe></Subscribe>
+      <Footer></Footer>
     </>
   );
-=======
-    return (
-        <>
-            <Navbar></Navbar>
-            <div className="flex md:gap-8">
-                <FilterSidebar></FilterSidebar>
-                <div className="md:w-9/12 w-8/12">
-                    <LayoutShift></LayoutShift>
-                    <Navigation></Navigation>
-                </div>
-            </div>
-            <Subscribe></Subscribe>
-            <Footer></Footer>
-        </>
-    )
->>>>>>> 27a5e5c30e8eeeb96fb7ed9d8e3d44bcfd246674
 }
 
 export default SecondPage;
