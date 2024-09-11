7165962537b9594ef5b1baf310edae9dd0d7b57c
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-11 13:19:30 +0000

    Added navbar on third page

---
+++ src/app/third-page/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
+
+function ThirdPage() {
+    return (
+        <>
+            <Navbar></Navbar>
+        </>
+    )
+}
+
+export default ThirdPage;
