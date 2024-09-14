47b1579fc9213d5920cf8214be7ed2a46e5b9d3a
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-14 16:44:53 +0000

    Added payment promotion section

--- src/app/fourth-page/page.tsx
+++ src/app/fourth-page/page.tsx
@@ -1,11 +1,13 @@
 import Navbar from "@/components/Navbar/Navbar";
 import ShoppingCart from "./ShoppingCart"
+import PaymentPromotion from "./PaymentPromotion";
 
 function FourthPage() {
     return (
         <>
             <Navbar></Navbar>
             <ShoppingCart></ShoppingCart>
+            <PaymentPromotion></PaymentPromotion>
         </>
     )
 }
