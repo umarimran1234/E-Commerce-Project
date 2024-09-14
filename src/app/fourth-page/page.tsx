1542d06f9d9cc73087e54fda940eeecdd2259d0a
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-14 16:20:49 +0000

    Added first section of fourth page

--- src/app/fourth-page/page.tsx
+++ src/app/fourth-page/page.tsx
@@ -1,9 +1,11 @@
 import Navbar from "@/components/Navbar/Navbar";
+import ShoppingCart from "./ShoppingCart"
 
 function FourthPage() {
     return (
         <>
             <Navbar></Navbar>
+            <ShoppingCart></ShoppingCart>
         </>
     )
 }
