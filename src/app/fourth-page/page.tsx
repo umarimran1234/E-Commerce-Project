75002cbd2065a1fec43589df6f149668cd25fe97
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-14 15:34:55 +0000

    Added navbar on fourth page

---
+++ src/app/fourth-page/page.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar/Navbar";
+
+function FourthPage() {
+    return (
+        <>
+            <Navbar></Navbar>
+        </>
+    )
+}
+
+export default FourthPage;
