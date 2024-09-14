0025dcd36beaac2dcfc1d563b10ccfc3bbca4d1c
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-09-14 17:18:04 +0000

    Fourth page completed

--- src/app/fourth-page/page.tsx
+++ src/app/fourth-page/page.tsx
@@ -2,6 +2,8 @@ import Navbar from "@/components/Navbar/Navbar";
 import ShoppingCart from "./ShoppingCart"
 import PaymentPromotion from "./PaymentPromotion";
 import SavedForLater from "./SavedForLater";
+import SuperDiscount from "../third-page/SuperDiscount";
+import Footer from "@/components/Footer/Footer";
 
 function FourthPage() {
     return (
@@ -10,6 +12,8 @@ function FourthPage() {
             <ShoppingCart></ShoppingCart>
             <PaymentPromotion></PaymentPromotion>
             <SavedForLater></SavedForLater>
+            <SuperDiscount></SuperDiscount>
+            <Footer></Footer>
         </>
     )
 }
