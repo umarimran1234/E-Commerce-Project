64c8f636e274044d34b5647aa0c7c37312b2d2a7
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-10-12 13:17:48 +0000

    Fixed banner section

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} bg-gray-100 ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
--- src/components/Banner/Banner.tsx
+++ src/components/Banner/Banner.tsx
@@ -1,73 +1,168 @@
-"use client"
-import { useState } from 'react';
-import { MdAccountCircle } from 'react-icons/md';
+"use client";
+import Image from "next/image";
+import { useState } from "react";
 
 const Banner: React.FC = () => {
-    const [activeTab, setActiveTab] = useState('Automobiles');
+  const [activeTab, setActiveTab] = useState("Automobiles");
 
-    const tabs = [
-        'Automobiles', 'Clothes and wear', 'Home interiors', 'Computer and tech',
-        'Tools, equipments', 'Sports and outdoor', 'Animal and pets', 'Machinery tools',
-        'More category'
-    ];
+  const tabs = [
+    "Automobiles",
+    "Clothes and wear",
+    "Home interiors",
+    "Computer and tech",
+    "Tools, equipments",
+    "Sports and outdoor",
+    "Animal and pets",
+    "Machinery tools",
+    "More category",
+  ];
 
-    const getContent = () => {
-        switch (activeTab) {
-            case 'Automobiles':
-                return 'Latest trending Electronic items'; // Replace with actual content
-            case 'Clothes and wear':
-                return 'Trending fashion items';
-            // Add cases for other tabs
-            default:
-                return 'No content available';
-        }
-    };
+  const getContent = () => {
+    switch (activeTab) {
+      case "Automobiles":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              Latest trending
+            </span>{" "}
+            <span className="text-[32px] font-bold">Electronic items</span>
+          </span>
+        ); // Replace with actual content
+      case "Clothes and wear":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              Clothes and wear
+            </span>
+          </span>
+        );
+      case "Home interiors":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              Home interiors
+            </span>
+          </span>
+        );
+      case "Computer and tech":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              Computer and tech
+            </span>
+          </span>
+        );
+      case "Tools, equipments":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              Tools, equipments
+            </span>
+          </span>
+        );
+      case "Sports and outdoor":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              Sports and outdoor
+            </span>
+          </span>
+        );
+      case "Animal and pets":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              Animal and pets
+            </span>
+          </span>
+        );
+      case "Machinery tools":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              Machinery tools
+            </span>
+          </span>
+        );
+      case "More category":
+        return (
+          <span className="flex flex-col">
+            <span className="text-[28px] text-[#1C1C1C] font-semibold">
+              More category
+            </span>
+          </span>
+        );
+      // Add cases for other tabs
+      default:
+        return "No content available";
+    }
+  };
 
-    return (
-        <div className='container mx-auto mt-10'>
-            <div className="flex flex-col md:flex-row md:justify-between border-2 rounded-lg border-gray-300 p-1">
-                {/* Sidebar */}
-                <aside className="w-full md:w-1/5 p-4">
-                    <ul>
-                        {tabs.map((tab) => (
-                            <li
-                                key={tab}
-                                className={`cursor-pointer p-2 ${activeTab === tab ? 'bg-blue-200 font-medium rounded-lg' : ''}`}
-                                onClick={() => setActiveTab(tab)}
-                            >
-                                {tab}
-                            </li>
-                        ))}
-                    </ul>
-                </aside>
+  return (
+    <div className="container mx-auto mt-10">
+      <div className="flex flex-col bg-white md:flex-row md:justify-between border-2 rounded-lg border-gray-200 p-1">
+        {/* Sidebar */}
+        <aside className="w-full md:w-1/5 p-4">
+          <ul>
+            {tabs.map((tab) => (
+              <li
+                key={tab}
+                className={`cursor-pointer p-2 ${
+                  activeTab === tab ? "bg-blue-200 font-medium rounded-lg" : ""
+                }`}
+                onClick={() => setActiveTab(tab)}
+              >
+                {tab}
+              </li>
+            ))}
+          </ul>
+        </aside>
 
-                {/* Main Content Area */}
-                <main className="w-full md:w-3/5 p-4">
-                    <div className="bg-green-200 p-6 rounded-lg bg-banner h-[20rem]">
-                        <h2 className="text-xl font-semibold bg-white text-black w-fit p-2 rounded-xl">{getContent()}</h2>
-                        <button className="bg-white text-black p-2 mt-4 rounded-md">Learn more</button>
-                    </div>
-                </main>
+        {/* Main Content Area */}
+        <main className="w-full md:w-3/5 p-4">
+          <div className="bg-green-200 p-10 rounded-lg bg-banner h-[26rem]">
+            <h2 className="">{getContent()}</h2>
+            <button className="bg-white text-black font-medium py-2 px-4 mt-4 rounded-md">
+              Learn more
+            </button>
+          </div>
+        </main>
 
-                {/* User/Promotion Panel */}
-                <aside className="w-full md:w-1/5 p-4 flex flex-col gap-4">
-                    <div className="bg-[#E3F0FF] p-4 rounded-lg">
-                        <h3 className='flex items-center gap-2'><MdAccountCircle className='text-4xl' /> {`Hi, user let's get started`}</h3>
-                        <div className='flex flex-col'>
-                            <button className="bg-[#127FFF] font-medimum text-white py-2 px-4 rounded-md mt-2">Join now</button>
-                            <button className="bg-white text-[#0D6EFD] font-medium py-2 px-4 rounded-md mt-2">Log in</button>
-                        </div>
-                    </div>
-                    <div className="bg-[#F38332] text-white p-4 rounded-lg">
-                        <p>Get US $10 off with a new supplier</p>
-                    </div>
-                    <div className="bg-[#55BDC3] text-white p-4 rounded-lg">
-                        <p>Send quotes with supplier preferences</p>
-                    </div>
-                </aside>
+        {/* User/Promotion Panel */}
+        <aside className="w-full md:w-1/5 p-4 flex flex-col gap-4">
+          <div className="bg-[#E3F0FF] p-4 rounded-lg">
+            <h3 className="flex items-center gap-2">
+              <Image
+                alt="User Image"
+                height={44}
+                width={44}
+                className="rounded-full"
+                src="/images/User.jpg"
+              ></Image>{" "}
+              <span className="flex flex-col">
+                <span>{`Hi, user`}</span>
+                <span>{`let's get started`}</span>
+              </span>
+            </h3>
+            <div className="flex flex-col">
+              <button className="bg-[#127FFF] font-medimum text-white py-2 px-4 rounded-md mt-2">
+                Join now
+              </button>
+              <button className="bg-white text-[#0D6EFD] font-medium py-2 px-4 rounded-md mt-2">
+                Log in
+              </button>
             </div>
-        </div>
-    );
+          </div>
+          <div className="bg-[#F38332] text-white p-4 rounded-lg">
+            <p className="flex flex-col"><span>Get US $10 off</span> <span>with a new</span> supplier</p>
+          </div>
+          <div className="bg-[#55BDC3] text-white p-4 rounded-lg">
+            <p className="flex flex-col"><span>Send quotes with</span> <span>supplier</span> preferences</p>
+          </div>
+        </aside>
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
