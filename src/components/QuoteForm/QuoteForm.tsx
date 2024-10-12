db93e9402050a6ecbc0196a922db97e8a4018647
MD ASHIKUR RAHMAN ASHIK <md.ashikur.upwork@gmail.com>
2024-10-12 17:36:54 +0000

    Made form section pixel perfect

--- src/components/QuoteForm/QuoteForm.tsx
+++ src/components/QuoteForm/QuoteForm.tsx
@@ -51,8 +51,8 @@ const QuoteForm: React.FC = () => {
 
     return (
         <div className="container mx-auto mt-[2rem]">
-            <Image src="/images/Stock.png" alt='Stock Image' className='relative' height={600} width={600}></Image>
-            <div className="bg-gradient-to-r absolute bottom-0 from-[#2C7CF1] to-[#00D1FF80] flex md:flex-row flex-col justify-center lg:gap-0 gap-8 md:gap-4 text-white rounded-lg py-8 px-8">
+            <Image src="/images/Stock.png" alt='Stock Image' className='relative h-[25.2rem] rounded-lg w-full' height={1000} width={1000}></Image>
+            <div className="bg-gradient-to-r absolute top-[108.5rem] w-[80rem] from-[#2C7CF1] to-[#00D1FF80] flex md:flex-row flex-col justify-center lg:gap-0 gap-8 md:gap-4 text-white rounded-lg py-8 px-8">
                 <div>
                     <h1 className="text-3xl lg:w-1/2 font-semibold">An easy way to send requests to all suppliers</h1>
                     <p className="mt-4 lg:w-1/2">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt.</p>
